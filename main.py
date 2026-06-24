21805a1b299b96691281c47c65456c92a3e733ef
Jakub Poczatek <kubapoczatek2001@gmail.com>
2026-06-24 20:11:21 +0100

    Exception Handling
    - Added basic exception handling.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy
 import time
 import wave
+import logging
 import sounddevice as sd
 from faster_whisper import WhisperModel
 from piper import PiperVoice
@@ -9,51 +10,46 @@ from wake import wait_for_wake_word
 
 # Needs to be 16khz for faster_whisper
 SAMPLE_RATE = 16000
-DURATION_SEC = 5
 CHANNELS = 1
 WHISPER_MODEL = "large-v3"
 WAV_PATH = "test.wav"
 KNOWN_COMMANDS = ["weather", "time"]
 
 def main():
-    whisperModel = WhisperModel(WHISPER_MODEL, device="cpu", compute_type="int8")
-    wakeWordModel = WakeWordModel(inference_framework="onnx", wakeword_models=["hey_jarvis"])
-    piperVoice = PiperVoice.load(r"voices\en_US-lessac-medium.onnx")
+    try:
+        whisperModel = WhisperModel(WHISPER_MODEL, device="cpu", compute_type="int8")
+        wakeWordModel = WakeWordModel(inference_framework="onnx", wakeword_models=["hey_jarvis"])
+        piperVoice = PiperVoice.load(r"voices\en_US-lessac-medium.onnx")
+    except Exception as e:
+        print(f"Failed to load models, exiting... \n{e}")
+        quit()
 
     while True:
-        wait_for_wake_word(wakeWordModel)
-        time.sleep(0.25)
-        listen(whisperModel, piperVoice)
+        try:
+            wait_for_wake_word(wakeWordModel)
+            time.sleep(0.25)
+            listen(whisperModel, piperVoice)
+        except Exception as e:
+            try:    
+                say("Something went wrong, restarting...", piperVoice)
+            except Exception as SayException:
+                print(f"Something went wrong while speaking... \n{SayException}")
+            print(f"Something went wrong, restarting... \n{e}")
     
 
 def listen(model, piperVoice):
     print("Recording...")
-    # recording = sd.rec(
-    #     frames=int(DURATION_SEC * SAMPLE_RATE),
-    #     samplerate=SAMPLE_RATE,
-    #     channels=CHANNELS,
-    #     dtype="float32"
-    # )
-    # sd.wait()
-    recording = record_until_silence()
 
+    recording = None
+    while (recording is None):
+        recording = record_until_silence()
+    
     print("Recording Stopped")
 
     segments, _ = model.transcribe(recording, beam_size=5, language="en")
-
-    print(segments)
-
-    {
-        # print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-
-        # for segment in segments:
-        #     print("[{} -> {}] {}".format(segment.start, segment.end, segment.text))
-    }
-
+    
     command = "".join(s.text for s in segments)
     command = command.lower()
-
-    print(command)
 
     commands = [c.strip() for c in command.split(" and ")]
     commands = [c for c in commands if any(kc in c for kc in KNOWN_COMMANDS)]
@@ -92,7 +88,7 @@ def record_until_silence():
                 break;
 
     recording = numpy.concatenate(chunks)
-    return recording
+    return recording if started_talking else None 
 
 def decide(commands):
     for c in commands:
